4e27debc5a9effdea2006c501f42b613de8b8fe9
Dubjude <84024893+Dubjude@users.noreply.github.com>
2022-07-14 22:03:38 +0200

    Do here whatever your heart desires

--- OpenGL 2D Engine/Application.cpp
+++ OpenGL 2D Engine/Application.cpp
@@ -10,10 +10,10 @@ int main()
 	renderer::createWindow(850, 550, "Test");
 	renderer::setWindowPos(1920 / 2 - 400, 1080 / 2 - 300);
 
-	/*mem::Texture_ptr ghost = renderer::loadTexture("ghost.png");
+	mem::Texture_ptr ghost = renderer::loadTexture("ghost.png");
 	mem::Texture_ptr popcat = renderer::loadTexture("pop_cat.png");
 	mem::Texture_ptr block = renderer::loadTexture("block.png");
-	mem::Texture_ptr brick = renderer::loadTexture("brick.png");*/
+	mem::Texture_ptr brick = renderer::loadTexture("brick.png");
 
 	mem::Font_ptr freeSans = renderer::defaultFont("ComicSans", 32);
 
@@ -28,10 +28,10 @@ int main()
 
 		renderer::fillBackground(glm::vec3(51));
 		
-		//renderer::drawRect({ 255, 255, 0 }, { 250, 250, 150, 150 });
-		//renderer::drawRect({ 0, 0, 255 }, mem::rotate({ 250, 250, 150, 150 }, mem::RectPoints::CENTER, angle));
+		renderer::drawRect({ 255, 255, 0 }, { 250, 250, 150, 150 });
+		renderer::drawRect({ 0, 0, 255 }, mem::rotate({ 250, 250, 150, 150 }, mem::RectPoints::CENTER, angle));
 		
-		/*
+		
 		if (renderer::getMouseButtonDown(MOUSE_BUTTON_RIGHT))
 			angle += 0.1;
 
@@ -39,13 +39,13 @@ int main()
 		renderer::drawTexture(popcat, { 50, 50, 450, 150 });
 		renderer::drawTexture(block, { 75, 350, 150, 150 });
 		renderer::drawTexture(popcat, mem::rotate({ 275, 315, 150, 150 }, mem::RectPoints::CENTER, angle));
-		*/
+
 
 		freeSans->draw("This is sample text", { 0, 0 }, 1.0f, { 0.5, 0.8f, 0.2f, 1.0f });
 		freeSans->draw("This is sample text", { 425, 275 }, 1.0f, { 1.0, 0.8f, 0.2f, 1.0f });
 		
-		//renderer::drawRect({255, 0, 0}, {0, 0, 250, 500});
-		/*mem::Renderer::drawCircle({ 200, 250, 0 }, { 400, 300 }, 150, 20);
+		renderer::drawRect({255, 0, 0}, {0, 0, 250, 500});
+		mem::Renderer::drawCircle({ 200, 250, 0 }, { 400, 300 }, 150, 20);
 		mem::Renderer::drawEllipse({ 0, 0, 250, 170 }, { 125, 350 }, 70, 30);
 		mem::Renderer::drawEllipse({ 0, 0, 250, 170 }, { 125, 350 }, 30, 120);
 		mem::Renderer::drawEllipse({ 0, 200, 250 }, { 125, 350 }, 100, 150, 5);
@@ -59,7 +59,7 @@ int main()
 		{
 			points.push_back({ 400 + radius * cos(angle), 300 + radius * sin(angle) });
 		}
-		mem::Renderer::drawPolygon({ 200, 0, 0 }, points, 10);*/
+		mem::Renderer::drawPolygon({ 200, 0, 0 }, points, 10);
 
 
 		mem::Renderer::update();
