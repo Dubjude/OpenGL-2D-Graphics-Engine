055b784dcb146746f0fe209439f7279ea1ee0589
Dubjude <84024893+Dubjude@users.noreply.github.com>
2022-07-19 23:06:19 +0200

    Add files via upload

--- OpenGL 2D Engine/Application.cpp
+++ OpenGL 2D Engine/Application.cpp
@@ -11,13 +11,26 @@ int main()
 	renderer::setWindowPos(1920 / 2 - 400, 1080 / 2 - 300);
 
 	mem::Texture_ptr ghost = renderer::loadTexture("ghost.png");
-	mem::Texture_ptr popcat = renderer::loadTexture("pop_cat.png");
+	/*mem::Texture_ptr popcat = renderer::loadTexture("pop_cat.png");
 	mem::Texture_ptr block = renderer::loadTexture("block.png");
-	mem::Texture_ptr brick = renderer::loadTexture("brick.png");
+	mem::Texture_ptr brick = renderer::loadTexture("brick.png");*/
+
+	//float angle = PI/5;
+
+	mem::Image img(250, 250, 4);
+	img.pixels = new unsigned char[img.width * img.height * 4];
+	for (int y = 0; y < img.height; y++) {
+		for (int x = 0; x < img.width; x++)
+		{
+			img.pixels[4 * (y * img.width + x) + 0] = 255;
+			img.pixels[4 * (y * img.width + x) + 1] = 0;
+			img.pixels[4 * (y * img.width + x) + 2] = 255;
+			img.pixels[4 * (y * img.width + x) + 3] = 125;
+		}
+	}
+	mem::Texture_ptr imTex = renderer::loadTexture(img);
 
 	mem::Font_ptr freeSans = renderer::defaultFont("ComicSans", 32);
-
-	float angle = PI/5;
 
 	while (!renderer::windowShouldClose())
 	{
@@ -28,24 +41,34 @@ int main()
 
 		renderer::fillBackground(glm::vec3(51));
 		
-		renderer::drawRect({ 255, 255, 0 }, { 250, 250, 150, 150 });
-		renderer::drawRect({ 0, 0, 255 }, mem::rotate({ 250, 250, 150, 150 }, mem::RectPoints::CENTER, angle));
+		//renderer::drawRect({ 255, 255, 0 }, { 250, 250, 150, 150 });
+		//renderer::drawRect({ 0, 0, 255 }, mem::rotate({ 250, 250, 150, 150 }, mem::RectPoints::CENTER, angle));
 		
 		
-		if (renderer::getMouseButtonDown(MOUSE_BUTTON_RIGHT))
-			angle += 0.1;
+		/*if (renderer::getMouseButtonDown(MOUSE_BUTTON_RIGHT))
+			angle += 0.1;*/
 
-		renderer::drawTexture(ghost, {550, 25, 200, 500}, {255, 100, 255});
-		renderer::drawTexture(popcat, { 50, 50, 450, 150 });
+		renderer::drawTexture(ghost, { 150, 25, 250, 250 });
+		renderer::drawTexture(imTex, { 550, 25, 250, 250 });
+		/*renderer::drawTexture(popcat, { 50, 50, 450, 150 });
 		renderer::drawTexture(block, { 75, 350, 150, 150 });
 		renderer::drawTexture(popcat, mem::rotate({ 275, 315, 150, 150 }, mem::RectPoints::CENTER, angle));
+		*/
 
-
-		freeSans->draw("This is sample text", { 0, 0 }, 1.0f, { 0.5, 0.8f, 0.2f, 1.0f });
-		freeSans->draw("This is sample text", { 425, 275 }, 1.0f, { 1.0, 0.8f, 0.2f, 1.0f });
-		
 		renderer::drawRect({255, 0, 0}, {0, 0, 250, 500});
-		mem::Renderer::drawCircle({ 200, 250, 0 }, { 400, 300 }, 150, 20);
+		renderer::drawRect({ 0, 255, 0 }, { 150, 300, 250, 100 });
+
+		renderer::drawText(freeSans, "This is sample text", { 0, 0 }, { 125, 200, 150, 255 });
+		renderer::drawText(freeSans, "This is sample text", { 250, 275 }, { 255, 200, 50, 125 });
+
+		float x = 750;
+		float y = 450;
+		for (int i = 0; i < 100; i++)
+		{
+			renderer::drawPixel(mem::Random::Vec3(255), {x + i, y});
+		}
+
+		/*mem::Renderer::drawCircle({ 200, 250, 0 }, { 400, 300 }, 150, 20);
 		mem::Renderer::drawEllipse({ 0, 0, 250, 170 }, { 125, 350 }, 70, 30);
 		mem::Renderer::drawEllipse({ 0, 0, 250, 170 }, { 125, 350 }, 30, 120);
 		mem::Renderer::drawEllipse({ 0, 200, 250 }, { 125, 350 }, 100, 150, 5);
@@ -59,9 +82,9 @@ int main()
 		{
 			points.push_back({ 400 + radius * cos(angle), 300 + radius * sin(angle) });
 		}
-		mem::Renderer::drawPolygon({ 200, 0, 0 }, points, 10);
+		mem::Renderer::drawPolygon({ 200, 0, 0 }, points, 10);*/
 
-
+		
 		mem::Renderer::update();
 	}
 
